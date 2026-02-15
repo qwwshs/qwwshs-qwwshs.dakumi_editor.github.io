019367a690c873d98f2f758c0880f98af9bd40ea
qwwshs <2559873038@qq.com>
2026-02-15 13:38:31 +0800

    deploy

--- bulid.bat
+++ bulid.bat
@@ -2,5 +2,5 @@
 
 git add -A
 git commit -m "deploy"
-git push -f git@github.com:qwwshs\qwwshs-qwwshs.dakumi_editor.github.io.git master:main
+git push -f git@github.com:qwwshs/qwwshs-qwwshs.dakumi_editor.github.io.git master:main
 cd -
